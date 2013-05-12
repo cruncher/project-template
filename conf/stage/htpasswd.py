c19b3112cf8c56eaef06063bfd1f0ffb6f8b6ea2
Marco Bonetti <mbonetti@gmail.com>
2013-05-12 23:11:36 +0200

    stage

--- conf/stage/htpasswd.py
+++
@@ -1,8 +0,0 @@
-# Script should be passed two paramters
-# username and password
-import crypt
-import sys
-
-if len(sys.argv) == 3:
-    print("%s:%s" % (sys.argv[1], crypt.crypt(sys.argv[2], sys.argv[2])))
-
