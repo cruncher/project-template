bf8277b84c92aeaf5a05fd54f980c4a7fe73fbd7
Marco Bonetti <mbonetti@gmail.com>
2016-05-28 16:01:43 +0200

    INTERNAL_IPS

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -49,7 +49,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-INTERNAL_IPS = ('127.0.0.1', )
+# INTERNAL_IPS = ('127.0.0.1', )
+INTERNAL_IPS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'tmp', 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'tmp', 'static')
