3166aed58c11f2fed34e320feb9a461cdc973d2f
Marco Bonetti <mbonetti@gmail.com>
2016-10-03 09:36:41 +0200

    Update urls.py

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,34 +1,36 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.views.generic.base import RedirectView
 from django.views.generic import TemplateView
 from django.contrib import admin
+from django.views.static import serve as static_serve
+
+from {{project_name}}.views import home
+
 admin.autodiscover()
 
+
 urlpatterns = i18n_patterns(
-    '',
-    url(r'^$', '{{project_name}}.views.home', name='home'),
-    url(r'^admin$', RedirectView.as_view(url='/admin/')),    
+    url(r'^$', home, name='home'),
+    url(r'^admin$', RedirectView.as_view(url='/admin/')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += patterns(
-    '',
+urlpatterns += [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^front-edit/', include('front.urls')),
-    # url(r'^redactor-3298hjahsd2/', include('apps.cruncher.urls')),
-)
+]
 
 if settings.DEBUG:
-    urlpatterns = patterns(
-        '',
+    urlpatterns = [
+
         url(r'^404.html', TemplateView.as_view(template_name='404.html')),
         url(r'^style/', include('apps.styleguide.urls')),
-        url(r'^favicon.ico$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT + '/img/', 'path': 'favicon.ico'}),
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'^favicon.ico$', static_serve, {'document_root': settings.STATIC_ROOT + '/img/', 'path': 'favicon.ico'}),
+        url(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         url(r'', include('django.contrib.staticfiles.urls')),
-    ) + urlpatterns
+    ] + urlpatterns
 
 if 'rosetta' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('', url(r'^rosetta/', include('rosetta.urls')), )
+    urlpatterns += [url(r'^rosetta/', include('rosetta.urls')), ]
