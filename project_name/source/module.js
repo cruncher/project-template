90ec7077c6992bae3d247880d39ea1190de7137d
stephband <stephband@googlemail.com>
2020-05-11 17:41:45 +0200

    Update document builder

---
+++ project_name/source/module.js
@@ -0,0 +1,15 @@
+
+// export { limit, last, cache, get } from './fn/module.js';
+// export { events, select, clone, request, attribute, next, append, before, rect, children, classes, closest, find, isTargetEvent, matches, previous, trigger } from './dom/module.js';
+
+import './dom/js/device.js';
+import './dom/js/fullscreenable.js';
+import './dom/js/swipeable.js';
+import './dom/js/switchable.js';
+import './dom/js/toggleable.js';
+import './dom/js/locateable.js';
+import './dom/js/validateable.js';
+
+import './bolt/components/switch-label.js';
+import './bolt/components/toggle-block.js';
+import './bolt/components/slides.js';
