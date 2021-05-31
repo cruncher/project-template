8b25a541b6f4cba0002688c1296a8fb465421031
Halit Celik <halit@cruncher.ch>
2021-05-31 11:38:11 +0200

    upgrade to use boto3 for s3 storage

--- project_name/project_name/settings/s3.py
+++ project_name/project_name/settings/s3.py
@@ -12,13 +12,13 @@
 </CORSConfiguration>
 """
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 
 AWS_STORAGE_BUCKET_NAME = ''
-AWS_S3_REGION_NAME = None
+AWS_S3_REGION_NAME = 'ch-gva-2'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_BUCKET_ACL = 'public-read'
 AWS_AUTO_CREATE_BUCKET = True
