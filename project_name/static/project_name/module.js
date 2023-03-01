37226fe3a294c60b7a79471043efaeefebe3034d
Marco Bonetti <marco@cruncher.ch>
2023-03-01 10:09:54 +0100

    base modules

---
+++ project_name/static/project_name/module.js
@@ -0,0 +1,16 @@
+// Elements
+import '../bolt/elements/dialog.js';
+import '../literal/elements/literal-element.js';
+import '../slide-show/module.js';
+import '../overflow-toggle/module.js';
+
+// Attributes
+export { messages as validationMessages } from '../bolt/attributes/data-validateable.js';
+import '../bolt/attributes/data-popable.js';
+import '../bolt/attributes/data-toggleable.js';
+import '../bolt/attributes/name=fullscreen.js';
+
+// Classes
+import '../bolt/classes/device.js';
+import '../bolt/classes/resizing.js';
+import '../bolt/classes/toggle-block.js';
