11f1e2203e6c655eae65ec89999e20030f465eb0
Marco Bonetti <marco@cruncher.ch>
2023-08-10 16:05:08 +0200

    New template loaders syntax

--- project_name/project_name/settings/cms.py
+++ project_name/project_name/settings/cms.py
@@ -67,6 +67,15 @@ TEMPLATES = [
                 "cms.context_processors.cms_settings",
             ],
             "debug": False,
+            "loaders": [
+                (
+                    'django.template.loaders.cached.Loader',
+                    [
+                        'django.template.loaders.filesystem.Loader',
+                        'django.template.loaders.app_directories.Loader',
+                    ],
+                ),
+            ],
         },
     }
 ]
