a85466e0f64905276f287cc13efc1a71c7f94caa
Marco Bonetti <marco@cruncher.ch>
2024-01-11 13:33:52 +0100

    Parler: copy_translations_on_save

--- project_name/apps/cruncher/admin.py
+++ project_name/apps/cruncher/admin.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from django.contrib import admin
 from django.utils.html import format_html_join
 
 
 class ParlerAllTranslationsMixin(object):
+    @admin.display(description="Translations")
     def all_translations(self, obj):
         _all_translations = [
             c.get("code") for c in settings.PARLER_LANGUAGES.get(settings.SITE_ID)
@@ -17,9 +19,7 @@ class ParlerAllTranslationsMixin(object):
             'href="{}/change/?language={}">{}</a>',
             [
                 (
-                    "rgb(94, 136, 157)"
-                    if lang in actual_codes
-                    else "rgb(149, 186, 204)",
+                    "rgb(94, 136, 157)" if lang in actual_codes else "rgb(149, 186, 204)",
                     obj.pk,
                     lang,
                     lang.upper(),
@@ -28,4 +28,18 @@ class ParlerAllTranslationsMixin(object):
             ],
         )
 
-    all_translations.short_description = "Translations"
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        if hasattr(self, "copy_translations_on_save") and self.copy_translations_on_save:
+            inst = form.instance
+            current_lang = request.GET.get("language")
+
+            translations = inst.translations.all()
+            current_translation = translations.filter(language_code=current_lang).first()
+
+            if inst and current_lang and current_translation:
+                for lc, _ in settings.LANGUAGES:
+                    if not translations.filter(language_code=lc).exists():
+                        current_translation.pk = None
+                        current_translation.language_code = lc
+                        current_translation.save()
