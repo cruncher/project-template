38e6c3aae48b4fab0219a51e7bc1ef8b5d2b349b
Marco Bonetti <marco@cruncher.ch>
2024-02-18 21:51:28 +0100

    Update s3.py

--- project_name/project_name/settings/s3.py
+++ project_name/project_name/settings/s3.py
@@ -12,6 +12,15 @@
 </CORSConfiguration>
 """
 
+
+THUMBNAIL_PROCESSORS = (
+    "easy_thumbnails.processors.colorspace",
+    "easy_thumbnails.processors.autocrop",
+    # "easy_thumbnails.processors.scale_and_crop",  # if no filer
+    "filer.thumbnail_processors.scale_and_crop_with_subject_location",
+    "easy_thumbnails.processors.filters",
+)
+
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
