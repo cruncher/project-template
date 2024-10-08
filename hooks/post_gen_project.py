c05c2844eacfc439afeb1b9371c6de57682c97aa
Halit Celik <halit@cruncher.ch>
2024-10-08 15:24:38 +0200

    use python3.12 for venv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -80,42 +80,42 @@ def main():
     exit_code = os.system("cd {{cookiecutter.project_slug}}")
     if exit_code:
         print("Project generation failed. Cannot cd in {{cookiecutter.project_slug}}")
-        return
-    exit_code = os.system("~/.pyenv/versions/3.12.*/bin/python -m venv .venv")
+        return exit_code
+    exit_code = os.system("python3.12 -m venv .venv")
     if exit_code:
         print("Project generation failed. Cannot create virtualenv")
-        return
+        return exit_code
     exit_code = os.system(".venv/bin/python -m pip install --upgrade pip wheel pip-tools")
     if exit_code:
         print("Project generation failed. Cannot install pip-tools")
-        return
+        return exit_code
     exit_code = os.system(".venv/bin/pip-compile")
     if exit_code:
         print("Project generation failed. Cannot run pip-compile")
-        return
+        return exit_code
     exit_code = os.system(".venv/bin/pip-sync")
     if exit_code:
         print("Project generation failed. Cannot run pip-sync")
-        return
+        return exit_code
     exit_code = os.system("cd {{cookiecutter.project_slug}}")
     if exit_code:
         print("Project generation failed. Cannot cd in {{cookiecutter.project_slug}}")
-        return
+        return exit_code
     exit_code = os.system("createdb {{cookiecutter.project_slug}}")
     if exit_code:
         print("Project generation failed. Cannot create db with name {{cookiecutter.project_slug}}")
-        return
+        return exit_code
     if exit_code:
         print("Project generation failed. Cannot set domain name in shell_plus")
-        return
+        return exit_code
     exit_code = os.system(".venv/bin/python {{cookiecutter.project_slug}}/manage.py migrate")
     if exit_code:
         print("Project generation failed. Cannot run migrate")
-        return
+        return exit_code
     exit_code = os.system("echo \"Site.objects.all().update(domain='{{cookiecutter.domain_name}}')\" | .venv/bin/python {{cookiecutter.project_slug}}/manage.py shell_plus")
     if exit_code:
         print("Project generation failed. Cannot set domain name in shell_plus")
-        return
+        return exit_code
     exit_code = os.system("echo \"User.objects.create_superuser('info@cruncher.ch','admin' )\" | .venv/bin/python {{cookiecutter.project_slug}}/manage.py shell_plus")
     print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
 
