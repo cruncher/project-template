ec258594b9f1f7636afb53948ea1e8862737fa19
Marco Bonetti <marco@cruncher.ch>
2025-11-21 14:43:13 +0100

    Update auth_backends.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/cruncher/auth_backends.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/cruncher/auth_backends.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
 from apps.users.models import User
 
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email="", password="", **kwargs):
-
-        users = User.objects.filter(email=email)
+    def authenticate(self, request, email="", username="", password="", **kwargs):
+        print(email, username, password, kwargs)
+        users = User.objects.filter(Q(email=email) | Q(email=username))
         for user in users:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
